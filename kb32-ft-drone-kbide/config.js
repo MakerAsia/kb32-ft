f901103d01cf65355c64dc186af19f2bffd18e72
Mr.Arnon Thongtem <hypermale@gmail.com>
2021-05-20 10:26:07 +0700

    add resources files

---
+++ kb32-ft-drone-kbide/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "kb32-ft",
+  vendor: "MakerAsia",
+  platform: "arduino-esp32",
+  title: "KB32-FT (Arduino API)",
+  description: "KB32-FT Compatible Kidbright32",
+  author: "Maker Asia",
+  website: "http://www.kidbright.club",
+  email: "info@makerasia.com",
+  git: "https://github.com/woodif/kb32-ft/",
+  image: "/static/display.png",
+  version: "1.0.8",
+  recommended: "ilovekbide",
+};
